9a1d4a2eec12749efe4b9e8656554053f6266d48
Christine Doza <cdoza@leoevents.com>
2021-10-20 11:01:12 -0500

    created title

--- src/components/MainPageLayout.js
+++ src/components/MainPageLayout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from "./Nav"
+import Title from "./Title"
 
 const MainPageLayout = ({ children }) => {
     return(
         <div>
       
           <Nav />
+          <Title title="Box Office" subtitle="Are you looking for a movie or an actor?" />
           { children }
             
         </div>
