74884a85226dee31f2a4ce186994db78eba2849b
Christine Doza <cdoza@leoevents.com>
2021-10-20 14:01:07 -0500

    display result and config file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,20 @@
 import React, {useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import { apiGet } from '../misc/config';
 
 const Home = () => {
 
     const [input, setInput] = useState("");
+    const [results, setResults] = useState(null);
 
     const onInputChange = (ev) => {
         setInput(ev.target.value);
     };
 
     const onSearch = () => {
-
-        fetch(`https://api.tvmaze.com/singlesearch/shows?q=${input}`)
-        .then(resp => resp.json())
+        apiGet(`/search/shows?q=${input}`)
         .then(result => {
-            console.log("result", result)
+            setResults(result)
         })
     };
 
@@ -24,10 +24,30 @@ const Home = () => {
         }
     };
 
+    const renderResults = () => {
+
+        if(results && results.length === 0){
+            return <div>No results</div>;
+        }
+
+        if(results && results.length > 0){
+            return ( 
+            <div> 
+                {results.map(item => (
+                <div key={item.show.id}>{item.show.name}</div>
+                ))}
+            </div>
+            );
+        }
+        return null;
+    }
+
     return (
         <MainPageLayout>
             <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
             <button type="button" onClick={onSearch}>Search</button>
+            {renderResults()}
+
         </MainPageLayout>
     )
 }
