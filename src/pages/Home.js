e17294171cec5c19ecd823f788698ba3b89fa9b5
Christine Doza <cdoza@leoevents.com>
2021-10-20 11:35:00 -0500

    connecting api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+
+    const [input, setInput] = useState("");
+
+    const onInputChange = (ev) => {
+        setInput(ev.target.value);
+    };
+
+    const onKeyDown = (ev) => {
+
+    }
+
+    const onSearch = () => {
+
+        fetch(`https://api.tvmaze.com/singlesearch/shows?q=${input}`)
+        .then(resp => resp.json())
+        .then(result => {
+            console.log(result)
+        })
+    };
+
     return (
         <MainPageLayout>
-            This is home
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+            <button type="button" onClick={onSearch}>Search</button>
         </MainPageLayout>
     )
 }
