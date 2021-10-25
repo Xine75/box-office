41666e6c6f4e2cb3e26d29b6d48f1c845cbd8a25
Christine Doza <cdoza@leoevents.com>
2021-10-25 10:04:36 -0500

    completed styling of showpage

--- src/components/show/ShowMainData.js
+++ src/components/show/ShowMainData.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import IMG_PLACEHOLDER from "../../images/not-found.png"
 import { Star } from '../styled';
-import { MainDataWrapper Headline } from './ShowMainData.styled';
+import { MainDataWrapper, Headline, TagList } from './ShowMainData.styled';
 
 
 const ShowMainData = ({ name, rating, summary, tags, image }) => {
@@ -18,14 +18,14 @@ const ShowMainData = ({ name, rating, summary, tags, image }) => {
           </Headline>
           <div className="summary" dangerouslySetInnerHTML={{ __html: summary }} />
   
-          <div>
+          <TagList>
             Tags:{' '}
             <div>
               {tags.map((tag, i) => (
                 <span key={i}>{tag}</span>
               ))}
             </div>
-          </div>
+          </TagList>
         </div>
       </MainDataWrapper>
     );
